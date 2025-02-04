66ef3edb197c56e594b3d9c4d8f71e447e2a2ab0
Owen Wienczkowski <owenwxyz@gmail.com>
2025-02-04 08:50:52 -0500

    dummy gpt structure

--- utility.py
+++ utility.py
@@ -235,7 +235,6 @@ input_embeddings = token_embeddings + pos_embeddings
 print("Input embeddings:\n",input_embeddings.shape)
 '''
 # begin basic self-attention mechanism
-import torch
 inputs = torch.tensor(
 [[0.43, 0.15, 0.89], # Your (x^1)
 [0.55, 0.87, 0.66], # journey (x^2)
@@ -576,8 +575,8 @@ batch_size, context_length, d_in = batch.shape
 d_out = 2
 mha = MultiHeadAttention(d_in, d_out, context_length, 0.0, num_heads=2)
 context_vecs = mha(batch)
-print(context_vecs)
-print("context_vecs.shape:", context_vecs.shape)
+# print(context_vecs)
+# print("context_vecs.shape:", context_vecs.shape)
 
 '''
 # Using the MultiHeadAttention class, initialize a multi-head attention
@@ -594,3 +593,67 @@ print(context_vecs_challenge)
 print("context_vecs_challenge.shape:", context_vecs_challenge.shape)
 '''
 
+# Implementing a GPT model from Scratch To Generate Text
+# Coding an LLM architecture:
+
+# Specify the configuration of a small GPT-2 model
+GPT_CONFIG_124M = {
+"vocab_size": 50257, # Vocabulary size
+"context_length": 1024, # Context length
+"emb_dim": 768, # Embedding dimension
+"n_heads": 12, # Number of attention heads
+"n_layers": 12, # Number of layers
+"drop_rate": 0.1, # Dropout rate
+"qkv_bias": False # Query-Key-Value bias
+}
+
+# a placeholder GPT backbone
+
+class DummyGPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+        self.trf_blocks = nn.Sequential(*[DummyTransformerBlock(cfg) for _ in range(cfg["n_layers"])]) 
+        self.final_norm = DummyLayerNorm(cfg["emb_dim"]) #B
+        self.out_head = nn.Linear(
+        cfg["emb_dim"], cfg["vocab_size"], bias=False)
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)
+        pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
+        x = tok_embeds + pos_embeds
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
+    
+class DummyTransformerBlock(nn.Module): #C
+    def __init__(self, cfg):
+        super().__init__()
+    def forward(self, x): #D
+        return x
+class DummyLayerNorm(nn.Module): #E
+    def __init__(self, normalized_shape, eps=1e-5): #F
+        super().__init__()
+    def forward(self, x):
+        return x
+
+# tokenize a batch consisting of two text inputs for the GPT model using the tiktoken tokenizer
+tokenizer = tiktoken.get_encoding("gpt2")
+batch = []
+txt1 = "Every effort moves you"
+txt2 = "Every day holds a"
+batch.append(torch.tensor(tokenizer.encode(txt1)))
+batch.append(torch.tensor(tokenizer.encode(txt2)))
+batch = torch.stack(batch, dim=0)
+# print(batch)
+
+# initialize a new 124 million parameter DummyGPTModel instance and feed it the tokenized batch
+torch.manual_seed(123)
+model = DummyGPTModel(GPT_CONFIG_124M)
+logits = model(batch)
+print("Output shape:", logits.shape)
+print(logits)
