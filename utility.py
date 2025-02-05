3cec8fde653af83a27cf137667caebf2df81a45e
Owen Wienczkowski <owenwxyz@gmail.com>
2025-02-05 18:31:59 -0500

    implemented layer normalizations

--- utility.py
+++ utility.py
@@ -655,5 +655,50 @@ batch = torch.stack(batch, dim=0)
 torch.manual_seed(123)
 model = DummyGPTModel(GPT_CONFIG_124M)
 logits = model(batch)
-print("Output shape:", logits.shape)
-print(logits)
+# print("Output shape:", logits.shape)
+# print(logits)
+
+# normalize layer outputs with ReLU
+torch.manual_seed(123)
+batch_example = torch.randn(2, 5) #A
+layer = nn.Sequential(nn.Linear(5, 6), nn.ReLU())
+out = layer(batch_example)
+# print(out)
+
+# examine normalization
+mean = out.mean(dim=-1, keepdim=True)
+var = out.var(dim=-1, keepdim=True)
+# print("Mean:\n", mean)
+# print("Variance:\n", var)
+
+# apply layer normalization to the layer outputs
+out_norm = (out - mean) / torch.sqrt(var)
+mean = out_norm.mean(dim=-1, keepdim=True)
+var = out_norm.var(dim=-1, keepdim=True)
+print("Normalized layer outputs:\n", out_norm)
+torch.set_printoptions(sci_mode=False)
+# print("Mean:\n", mean)
+# print("Variance:\n", var)
+
+# layer normalization class
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5 # small constant to prevent dividing by zero 
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+
+ln = LayerNorm(emb_dim=5)
+out_ln = ln(batch_example)
+mean = out_ln.mean(dim=-1, keepdim=True)
+var = out_ln.var(dim=-1, unbiased=False, keepdim=True)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+print(ln)
+ln.forward(10)
+print(ln)
