1ff2929b5d3eb09cb4e0d4416fe5c5da0274965f
Owen Wienczkowski <owenwxyz@gmail.com>
2025-02-17 19:25:17 -0500

    Decoding strategies to control randomness implemented: temperature, top-k sampling

--- utility.py
+++ utility.py
@@ -1211,16 +1211,16 @@ def generate_and_print_sample(model, tokenizer, device, start_context):
     print(decoded_text.replace("\n", " "))  # Compact print format
     model.train()
 
-torch.manual_seed(123)
-model = GPTModel(GPT_CONFIG_124M)
-model.to(device)
-optimizer = torch.optim.AdamW(model.parameters(), lr=0.0004, weight_decay=0)
-num_epochs = 10
-train_losses, val_losses, tokens_seen = train_model_simple(
-    model, train_loader, val_loader, optimizer, device,
-    num_epochs=num_epochs, eval_freq=5, eval_iter=1,
-    start_context="Every effort moves you", tokenizer=tokenizer
-)
+# torch.manual_seed(123)
+# model = GPTModel(GPT_CONFIG_124M)
+# model.to(device)
+# optimizer = torch.optim.AdamW(model.parameters(), lr=0.0004, weight_decay=0)
+# num_epochs = 10
+# train_losses, val_losses, tokens_seen = train_model_simple(
+#     model, train_loader, val_loader, optimizer, device,
+#     num_epochs=num_epochs, eval_freq=5, eval_iter=1,
+#     start_context="Every effort moves you", tokenizer=tokenizer
+# )
 
 # create a graph of loss over epochs
 from matplotlib.ticker import MaxNLocator
@@ -1244,5 +1244,212 @@ def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
     plt.savefig("loss-plot.pdf")
     plt.show()
 
-epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
-plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
+# epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
+# plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
+
+# Decoding strategies to control randomness
+
+# transfer the model back from the GPU to the CPU since
+# inference with a relatively small model does not require a GPU
+# model.to("cpu")
+# model.eval()
+
+# tokenizer = tiktoken.get_encoding("gpt2")
+# token_ids = generate_text_simple(
+#     model=model,
+#     idx=text_to_token_ids("Every effort moves you", tokenizer),
+#     max_new_tokens=25,
+#     context_size=GPT_CONFIG_124M["context_length"]
+# )
+# print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
+# Temperature scaling
+
+vocab = {
+    "closer": 0,
+    "every": 1,
+    "effort": 2,
+    "forward": 3,
+    "inches": 4,
+    "moves": 5,
+    "pizza": 6,
+    "toward": 7,
+    "you": 8,
+}
+inverse_vocab = {v: k for k, v in vocab.items()}
+
+next_token_logits = torch.tensor(
+    [4.51, 0.89, -1.90, 6.75, 1.63, -1.62, -1.89, 6.28, 1.79]
+)
+
+probas = torch.softmax(next_token_logits, dim=0)
+next_token_id = torch.argmax(probas).item()
+# print(inverse_vocab[next_token_id])
+
+torch.manual_seed(123)
+next_token_id = torch.multinomial(probas, num_samples=1).item()
+# print(inverse_vocab[next_token_id])
+
+def print_sampled_tokens(probas):
+    torch.manual_seed(123) # Manual seed for reproducibility
+    sample = [torch.multinomial(probas, num_samples=1).item() for i in range(1_000)]
+    sampled_ids = torch.bincount(torch.tensor(sample))
+    for i, freq in enumerate(sampled_ids):
+        print(f"{freq} x {inverse_vocab[i]}")
+
+# print_sampled_tokens(probas)
+
+def softmax_with_temperature(logits, temperature):
+    scaled_logits = logits / temperature
+    return torch.softmax(scaled_logits, dim=0)
+
+temperatures = [1, 0.1, 5] # Original, higher, and lower temperature
+scaled_probas = [softmax_with_temperature(next_token_logits, T) for T in temperatures]
+# x = torch.arange(len(vocab))
+# bar_width = 0.15
+# fig, ax = plt.subplots(figsize=(5, 3))
+# for i, T in enumerate(temperatures):
+#     rects = ax.bar(x + i * bar_width, scaled_probas[i],
+#         bar_width, label=f'Temperature = {T}')
+# ax.set_ylabel('Probability')
+# ax.set_xticks(x)
+# ax.set_xticklabels(vocab.keys(), rotation=90)
+# ax.legend()
+# plt.tight_layout()
+# plt.show()
+
+
+# Exercise 5.1
+# Use the print_sampled_tokens function to print the sampling frequencies of
+# the softmax probabilities scaled with the temperatures shown in Figure 5.13.
+# How often is the word "pizza" sampled in each case? Can you think of a
+# faster and more accurate way to determine how often the word "pizza" is sampled?
+
+# for _ in range(len(temperatures)):
+#     print_sampled_tokens(scaled_probas[_])
+#     print(scaled_probas[_][6])
+#     # print(inverse_vocab[6])
+
+
+#  top-k sampling
+top_k = 3
+top_logits, top_pos = torch.topk(next_token_logits, top_k)
+# print("Top logits:", top_logits)
+# print("Top positions:", top_pos)
+
+new_logits = torch.where(
+    condition=next_token_logits < top_logits[-1], #A
+    input=torch.tensor(float('-inf')), #B
+    other=next_token_logits #C
+)
+# print(new_logits)
+
+topk_probas = torch.softmax(new_logits, dim=0)
+# print(topk_probas)
+
+# implement combination of temperature and top-k samplilng into generate function
+def generate(model, idx, max_new_tokens, context_size, temperature=0.0, top_k=None, eos_id=None):
+
+    # For-loop is the same as before: Get logits, and only focus on last time step
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+
+        # New: Filter logits with top_k sampling
+        if top_k is not None:
+            # Keep only top_k values
+            top_logits, _ = torch.topk(logits, top_k)
+            min_val = top_logits[:, -1]
+            logits = torch.where(logits < min_val, torch.tensor(float("-inf")).to(logits.device), logits)
+
+        # New: Apply temperature scaling
+        if temperature > 0.0:
+            logits = logits / temperature
+
+            # Apply softmax to get probabilities
+            probs = torch.softmax(logits, dim=-1)  # (batch_size, context_len)
+
+            # Sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1)  # (batch_size, 1)
+
+        # Otherwise same as before: get idx of the vocab entry with the highest logits value
+        else:
+            idx_next = torch.argmax(logits, dim=-1, keepdim=True)  # (batch_size, 1)
+
+        if idx_next == eos_id:  # Stop generating early if end-of-sequence token is encountered and eos_id is specified
+            break
+
+        # Same as before: append sampled index to the running sequence
+        idx = torch.cat((idx, idx_next), dim=1)  # (batch_size, num_tokens+1)
+
+    return idx
+
+torch.manual_seed(123)
+
+# token_ids = generate(
+#     model=model,
+#     idx=text_to_token_ids("Every effort moves you", tokenizer),
+#     max_new_tokens=15,
+#     context_size=GPT_CONFIG_124M["context_length"],
+#     top_k=25,
+#     temperature=1.4
+# )
+
+# print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
+# Exercise 5.2
+# Play around with different temperatures and top-k settings. Based on your
+# observations, can you think of applications where lower temperature and topk settings are desired? Vice versa, can you think of applications where higher
+# temperature and top-k settings are preferred? (It's recommended to also
+# revisit this exercise at the end of the chapter after loading the pretrained
+# weights from OpenAI.)
+
+# token_ids = generate(
+#     model=model,
+#     idx=text_to_token_ids("Every effort moves you", tokenizer),
+#     max_new_tokens=15,
+#     context_size=GPT_CONFIG_124M["context_length"],
+#     top_k=3,
+#     temperature=0.09
+# )
+
+# print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
+# token_ids = generate(
+#     model=model,
+#     idx=text_to_token_ids("Every effort moves you", tokenizer),
+#     max_new_tokens=15,
+#     context_size=GPT_CONFIG_124M["context_length"],
+#     top_k=30,
+#     temperature=4.5
+# )
+
+# print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
+# Lower values of topk and temperature may be more desirable in situation where highly specific/accurate responses are critical
+# Higher values of topk and temperature may be more valuable in creative writing or storytelling circumstances
+
+
+# Exercise 5.3
+# What are the different combinations of settings for the generate function to
+# force deterministic behavior, that is, disabling the random sampling such that
+# it always produces the same outputs similar to the generate_simple function?
+
+# the settings to disable random sampling are related to both temperature and topk
+# we can produce the same output as the generate_simple function through these settings:
+# topk = None
+# temperature = 1
+# the code with these settings is below
+token_ids = generate(
+    model=model,
+    idx=text_to_token_ids("Every effort moves you", tokenizer),
+    max_new_tokens=15,
+    context_size=GPT_CONFIG_124M["context_length"],
+    top_k=None,
+    temperature=1
+)
+print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
+# An alternative method is to set topk=1. This will have only the most likely token generated
